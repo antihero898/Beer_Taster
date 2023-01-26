390f0af0273d448c9e737fdbbffa74ddf7d553ce
Greg Humistion <antihero989@gmail.com>
2023-01-25 23:00:52 -0600

    adjust data for realistic id's, add todo

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from './App'
 import './index.css'
 
 
+// 1. attempt to integrate WebSQL, AWS Lambda (AWS DynamoDB), and MongoDB
+
 // TODO: eventually integrate typescript
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+);  
