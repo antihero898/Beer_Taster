a41bd3c817ab84e92147d86edff66ae849aa77eb
Greg Humistion <antihero989@gmail.com>
2023-02-01 15:13:29 -0600

    remove unused files

--- adapter.js
+++
@@ -1,8 +0,0 @@
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
-
-export const db = () => {
-    return new Low(new JSONFile('/Users/greghumiston/projects/Beer_Taster/public/beers.json'));
-};
-
-export default db;
